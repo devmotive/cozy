aef230bcc4a20e3df42a461124c1059d01ad0788
devmotive <ahmad.codespace@gmail.com>
2025-08-12 10:46:40 +0700

    feat: Add color palette

---
+++ lib/theme/colors.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+// Brand Colors
+const Color kOrangeColor = Color(0xFFFF9376);
+const Color kPurpleColor = Color(0xFF5843BE);
+const Color kSoftPurpleColor = Color(0xFF7F74EB);
+const Color kDarkPurpleColor = Color(0xFF3A3379);
+const Color kYellowColor = Color(0xFFF4AD1D);
+const Color kRedColor = Color(0xFFFF5B5B);
+
+// Neutral Colors
+const Color kWhiteColor = Color(0xFFFFFFFF);
+const Color kBlackColor = Color(0xFF000000);
+
+// Grey Shades
+const Color kGreyColor = Color(0xFF82868E);
+const Color kDarkGreyColor = Color(0xFF7A7E86);
+const Color kLightGreyColor = Color(0xFFF6F7F8);
+const Color kMediumGreyColor = Color(0xFF989BA1);
+const Color kSilverColor = Color(0xFFC9C9C9);
